48c77152a063727caa483715747fd72ef0c5d230
Jnanika <jyothi.c.hulmani@gmail.com>
2020-06-07 14:26:51 -0500

    Testing adding JavaProgj to GIT

---
+++ src/main/java/com/itexps/employee/EmployeeFirstLastName.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.employee;
+
+/**
+ *
+ * @author manju
+ */
+public class EmployeeFirstLastName {
+    
+    String fname;
+    String lname;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+    
+    
+}
---
+++ src/test/java/com/itexps/employee/TestEmployeeFname.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.employee;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author manju
+ */
+public class TestEmployeeFname {
+    EmployeeFirstLastName emp;
+    
+    public TestEmployeeFname() {
+        emp= new EmployeeFirstLastName();
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testFirstName(){
+        emp.setFname("Mann");
+        assertEquals("Mann", emp.getFname());
+    }
+}
